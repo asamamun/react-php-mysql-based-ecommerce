21f253dcf16e8b00f7527c949c02206fde5a3b48
asamamun <asamamun@gmail.com>
2024-04-24 10:46:35 +0600

    added React Helmet to add or update head tags content

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { useCart } from './../CartContext';
+import { Helmet } from 'react-helmet';
 export const Cart = () => {
     const { cart, removeFromCart } = useCart();
     return (
         <div>
+            <Helmet>
+                <title>Cart Details</title>
+                <meta name="description" content='Cart Details WITH helmet' />
+            </Helmet>
             <h1>Cart details</h1>
             <table className="table table-primary table-striped table-hover">
                 <thead>
@@ -15,16 +20,16 @@ export const Cart = () => {
                     </tr>
                 </thead>
                 <tbody>
-                {
-                    cart.map((item,index) => (
-                        <tr key={index}>
-                            <td>{item.name}</td>
-                            <td>{item.quantity}</td>
-                            <td>{item.price}</td>
-                            <td><button className="btn btn-danger" onClick={() => removeFromCart(index)}>Remove</button></td>
-                        </tr>
-                    ))
-                }
+                    {
+                        cart.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.name}</td>
+                                <td>{item.quantity}</td>
+                                <td>{item.price}</td>
+                                <td><button className="btn btn-danger" onClick={() => removeFromCart(index)}>Remove</button></td>
+                            </tr>
+                        ))
+                    }
                 </tbody>
             </table>
         </div>
